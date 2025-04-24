b75f4cfe72e38345a302ee2166bf3f6a2740a9cc
Szkárosi Szilárd (DLWGQZ) <90700865+szkarosisz1@users.noreply.github.com>
2025-04-24 08:22:29 +0200

    Feat: Implementáltam 3D-s B-spline görbe kirajzolását gömbi koordinátákból, interaktív pontmegadással és animációval.

--- Beadando/b_spline_spherical_coordinates.py
+++ Beadando/b_spline_spherical_coordinates.py
@@ -1,6 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.interpolate import BSpline
+from mpl_toolkits.mplot3d import Axes3D
+import random
+import matplotlib.animation as animation
 
 def spherical_to_cartesian(r, theta, phi):
     """Átalakítás gömbi koordinátákból derékszögű koordinátákba"""
@@ -21,15 +24,23 @@ while True:
         print("Érvénytelen szám, próbáld újra!")
 
 control_points = []
-print("Add meg a kontrollpontokat (r, theta, phi) formában!")
-for i in range(num_points):
-    while True:
-        try:
-            r, theta, phi = map(float, input(f"Pont {i+1}: ").split())
-            control_points.append([r, theta, phi])
-            break
-        except ValueError:
-            print("Hibás formátum! Kérlek, három számot adj meg szóközzel elválasztva!")
+use_random = input("Szeretnéd véletlenszerűen generálni a pontokat? (igen/nem): ").strip().lower()
+if use_random == "igen":
+    for _ in range(num_points):
+        r = 1  # Sugár fixen 1
+        theta = random.uniform(0, 2 * np.pi)
+        phi = random.uniform(0, np.pi)
+        control_points.append([r, theta, phi])
+else:
+    print("Add meg a kontrollpontokat (r, theta, phi) formában!")
+    for i in range(num_points):
+        while True:
+            try:
+                r, theta, phi = map(float, input(f"Pont {i+1}: ").split())
+                control_points.append([r, theta, phi])
+                break
+            except ValueError:
+                print("Hibás formátum! Kérlek, három számot adj meg szóközzel elválasztva!")
 
 # B-spline csomópontok generálása
 k = 3  # fok
@@ -44,12 +55,20 @@ curve_points = np.array([spl(t) for t in t_values])
 
 # Átalakítás derékszögű koordinátákra
 cartesian_points = np.array([spherical_to_cartesian(r, theta, phi) for r, theta, phi in curve_points])
+control_cartesian = np.array([spherical_to_cartesian(*p) for p in control_points])
 
 # 3D megjelenítés
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
 ax.plot(cartesian_points[:, 0], cartesian_points[:, 1], cartesian_points[:, 2], label="B-spline görbe")
-ax.scatter(*zip(*[spherical_to_cartesian(*p) for p in control_points]), color='red', label='Kontrollpontok')
+ax.scatter(control_cartesian[:, 0], control_cartesian[:, 1], control_cartesian[:, 2], color='red', label='Kontrollpontok')
+
+# Érintővektorok számítása
+for i in range(len(t_values) - 1):
+    p1 = cartesian_points[i]
+    p2 = cartesian_points[i + 1]
+    tangent = (p2 - p1) * 0.2  # Méretezés
+    ax.quiver(p1[0], p1[1], p1[2], tangent[0], tangent[1], tangent[2], color='green', length=0.2)
 
 ax.set_xlabel('X')
 ax.set_ylabel('Y')
@@ -57,4 +76,17 @@ ax.set_zlabel('Z')
 ax.set_title("B-spline görbe gömbi koordinátákban")
 ax.legend()
 
+# Animáció létrehozása
+def update(num, data, line):
+    line.set_data(data[:num, :2].T)
+    line.set_3d_properties(data[:num, 2])
+    return line,
+
+data = np.array(cartesian_points)
+line, = ax.plot([], [], [], 'r', label="B-spline görbe")
+ani = animation.FuncAnimation(fig, update, frames=len(data), fargs=(data, line), interval=50, blit=False)
+
 plt.show()
+
+# Összehasonlítás/Tanulság: Descartes és Gömbi (szebb, érvek, hol érdemes használni)
+
